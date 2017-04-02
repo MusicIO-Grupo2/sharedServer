6431a22c46796a4aa4c0b0b532758843d7111547
NF <nflabo@gmail.com>
2017-04-02 20:16:11 -0300

    fix deploy en heroku

--- bin/exec.js
+++ bin/exec.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 var app = require('../app');
-app.set('port', (process.env.PORT || 5000));
 
-// Start listening for HTTP requests
-app.listen(app.get('port'),'localhost', function() {
-console.log('Node app is running on port', app.get('port'));
-	});
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
