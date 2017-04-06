5ba74cc943fcd34be20ac2d577637a7a04dce00d
nf <nflabo@gmail.com>
2017-04-06 00:15:09 -0300

    Refactor carpetas

--- models/index.js
+++ models/index.js
@@ -5,9 +5,10 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config.json')[env];
+var config    = require(__dirname + '/config.json')[env];
 var db        = {};
 
+console.log(process.env);
 console.log(config);
 console.log(env);
 
